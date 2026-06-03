a2d96cd1ee7a8490fa29141dcc0641014d733fad
martina-asistant <sheilacalpegerman@gmail.com>
2026-06-03 12:38:03 +0200

    Update layout.tsx

--- app/(panel)/layout.tsx
+++ app/(panel)/layout.tsx
@@ -1,9 +1,10 @@
 import Sidebar from '@/components/layout/Sidebar';
 
 const PanelLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="min-h-screen bg-[#020F14] text-white">
+  <div className="min-h-screen bg-[#020F14] text-white pb-6">
     <Sidebar />
-    <main className="h-[calc(100vh-110px)] overflow-hidden">
+
+    <main className="h-[calc(100vh-128px)] overflow-hidden">
       {children}
     </main>
   </div>
